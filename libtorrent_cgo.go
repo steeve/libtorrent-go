ffaeeb1a92ec1fced6ec8181585f0089a2ce1cd4
Steeve Morin <steeve.morin@gmail.com>
2014-11-17 18:40:28 +0100

    Remove debug flags in cgo
    Signed-off-by: Steeve Morin <steeve.morin@gmail.com>

--- libtorrent_cgo.go
+++ libtorrent_cgo.go
@@ -3,6 +3,6 @@ package libtorrent
 // #cgo pkg-config: libtorrent-rasterbar openssl
 // #cgo darwin LDFLAGS: -lm -lstdc++
 // #cgo linux LDFLAGS: -lm -lstdc++ -ldl -lrt
-// #cgo windows CXXFLAGS: -DIPV6_TCLASS=39 -O0 -g
+// #cgo windows CXXFLAGS: -DIPV6_TCLASS=39
 // #cgo windows LDFLAGS: -static-libgcc -static-libstdc++
 import "C"
